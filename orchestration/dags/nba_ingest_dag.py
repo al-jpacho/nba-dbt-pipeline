e0352611ba8f77808be49276ba166281fa46523e
al-jpacho <jordanapacho@gmail.com>
2025-06-19 21:57:20 +0100

    chore(orchestration): set up data ingestion DAG

---
+++ orchestration/dags/nba_ingest_dag.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+DEFAULT_ARGS = {
+    "owner": "aljpacho",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="nba_data_ingestion",
+    default_args=DEFAULT_ARGS,
+    description="Run nba_api ingestion script and store in DuckDB",
+    schedule_interval=None,
+    start_date=datetime(2024, 6, 1),
+    catchup=False,
+    tags=["nba", "ingestion", "duckdb", "dbt"],
+) as dag:
+    
+    ingest_nba_data = BashOperator(
+        task_id="run_ingest_script",
+        bash_commaned="python /opt/airflow/dags/../../scripts/ingest_nba_data.up"
+    )
